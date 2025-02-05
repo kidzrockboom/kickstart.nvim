02c37251d9cc75503678d7f2c941c384ac1a6bea
Richard Obaze <richardobaze@yahoo.co.uk>
2025-02-05 23:01:39 +0000

    Fixed undo tree error

--- lua/custom/plugins/telescope-undo.lua
+++ lua/custom/plugins/telescope-undo.lua
@@ -25,12 +25,12 @@ return {
         -- no other extensions here, they can have their own spec too
       },
     },
-    config = function(_, opts)
-      -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
-      -- configs for us. We won't use data, as everything is in it's own namespace (telescope
-      -- defaults, as well as each extension).
-      require('telescope').setup(opts)
-      require('telescope').load_extension 'undo'
-    end,
   },
+  config = function(_, opts)
+    -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
+    -- configs for us. We won't use data, as everything is in it's own namespace (telescope
+    -- defaults, as well as each extension).
+    require('telescope').setup(opts)
+    require('telescope').load_extension 'undo'
+  end,
 }
